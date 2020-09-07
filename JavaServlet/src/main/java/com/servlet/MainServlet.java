a39f59342973dce596cd38779d2b759d7ee9868b
Giovannie Mendoza <aztrus3@yahoo.com>
2020-09-06 19:19:56 -0700

    Redesigned code structure and added feature to handle articles that are found in multiple categories

--- JavaServlet/src/main/java/com/servlet/MainServlet.java
+++ JavaServlet/src/main/java/com/servlet/MainServlet.java
@@ -25,34 +25,31 @@ public class MainServlet extends HttpServlet {
     private final String baseURL = "https://time.com";
     private final String[] timeMagSections = {"/section/tech", "/section/sports", "/section/business", "/section/science"};
     private final String[] timeMagCategories = {"Tech", "Sports", "Business", "Science"};
-    private List<NewsItem> newsItems;
-    private boolean isCached = false;
     private final DateTimeFormatter ISOFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH);
+    private List<NewsItem> newsItems = new ArrayList<>();
+    private Map<String, Integer> pathMap = new HashMap<>();
+    private boolean isCached = false;
+
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //Setting up response
         response.setContentType("application/json");
         response.setCharacterEncoding("UTF-8");
         response.addHeader("Access-Control-Allow-Origin", "*");
         PrintWriter out = response.getWriter();
 
-        //Check for cached data to return
-        if(isCached)
-        {
+        //Check if data is cached
+        if(isCached) {
             out.print(convertToJSON(newsItems));
             out.flush();
         }
+        //Otherwise scrape data
         else {
-
-            newsItems = new ArrayList<NewsItem>();
-            int sectionsLen = timeMagSections.length;
-            //Go through each Time Section and add scraped data to list
-            for (int i = 0; i < sectionsLen; i++) {
-                newsItems.addAll(scrapeUrl(baseURL + timeMagSections[i], timeMagCategories[i]));
-            }
-
-            //Sort each news item by its published time
+            //Scrape data from Time.com
+            scrapeUrl();
+            //Sort each newsItem by its corresponding articles published time
             newsItems.sort(Comparator.comparing(NewsItem::getPublishedDate));
-            //Reverse list to have the most recent news articles first
+            //Reverse list to have the most recent newsItem first
             Collections.reverse(newsItems);
             //Set flag for next request to use cached data
             isCached = true;
@@ -62,49 +59,57 @@ public class MainServlet extends HttpServlet {
     }
 
     /* Converts NewsItem list to a JSON string */
-    protected String convertToJSON(List<NewsItem> newsItems)
-    {
-        // Convert NewsItem list to a JSON string
+    protected String convertToJSON(List<NewsItem> newsItems) {
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
         return gson.toJson(newsItems);
     }
 
     /*Uses url to connect and scrape key information*/
-    protected List<NewsItem> scrapeUrl(String url, String category) {
-        try {
-            //Fetch the website and store it in Document object
-            Document doc = Jsoup.connect(url).get();
+    protected void scrapeUrl() {
+        int sectionsLen = timeMagSections.length;
+        for (int i = 0; i < sectionsLen; i++) {
+            try {
+                //Fetch the website and store it in Document object
+                Document doc = Jsoup.connect(baseURL + timeMagSections[i]).get();
 
-            //Retrieve all articles from Document object
-            Elements articles = doc.getElementsByTag("article");
+                //Retrieve all articles from Document object
+                Elements articles = doc.getElementsByTag("article");
 
-            List newsItems = new ArrayList<NewsItem>();
-            NewsItem newsItem = null;
-            //Go through each article and scrape key data
-            for (Element article: articles)
-            {
-                newsItem = new NewsItem();
+                NewsItem newsItem = null;
 
-                Elements articleTitleURL = article.select("h3");
-                Elements articleExcerpt = article.getElementsByClass("summary margin-8-bottom desktop-only");
-                Elements articleImgURL = article.getElementsByClass("media-img margin-16-bottom");
+                //Go through each article tag and scrape key data
+                for (Element article : articles) {
+                    newsItem = new NewsItem();
 
-                newsItem.setCategory(category);
-                newsItem.setTitle(articleTitleURL.select("a").text());
-                newsItem.setPageUrl(baseURL + articleTitleURL.select("a").attr("href"));
-                newsItem.setExcerpt(articleExcerpt.text());
-                newsItem.setImgUrl(articleImgURL.select("div").attr("data-src"));
-		        newsItem.setPublishedDate(articlePublishedDate(newsItem.getPageUrl()));
+                    Elements articleTitleURL = article.select("h3");
+                    Elements articleExcerpt = article.getElementsByClass("summary margin-8-bottom desktop-only");
+                    Elements articleImgURL = article.getElementsByClass("media-img margin-16-bottom");
 
-                newsItems.add(newsItem);
+                    //Checking for duplicate article
+                    if (pathMap.containsKey(articleTitleURL.select("a").attr("href"))) {
+                        int newsItemPos = pathMap.get(articleTitleURL.select("a").attr("href"));
+                        String currentCategory = newsItems.get(newsItemPos).getCategory();
+                        newsItems.get(newsItemPos).setCategory(currentCategory + "," + timeMagCategories[i]);
+                    }
+                    //Otherwise creating new newsItem
+                    else {
+                        newsItem.setCategory(timeMagCategories[i]);
+                        newsItem.setTitle(articleTitleURL.select("a").text());
+                        newsItem.setPageUrl(baseURL + articleTitleURL.select("a").attr("href"));
+                        newsItem.setExcerpt(articleExcerpt.text());
+                        newsItem.setImgUrl(articleImgURL.select("div").attr("data-src"));
+                        newsItem.setPublishedDate(articlePublishedDate(newsItem.getPageUrl()));
+
+                        newsItems.add(newsItem);
+                        //Mapping current articles url path to its corresponding newsItem object to handle duplicates
+                        pathMap.put(articleTitleURL.select("a").attr("href"), newsItems.size() - 1);
+                    }
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            return newsItems;
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
         }
-
     }
 
     /* Retrieves a specified article's published date and time */
@@ -115,6 +120,7 @@ public class MainServlet extends HttpServlet {
 
             Element articleDate = doc.getElementById("page-gtm-values");
 
+            //Scrape date and time in ISO format and convert to LocalDateTime object
             return LocalDateTime.parse(articleDate.select("div").attr("data-content_published_date"), ISOFormatter);
 
         } catch (IOException e) {
